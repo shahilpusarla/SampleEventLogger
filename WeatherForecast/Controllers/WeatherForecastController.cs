0029bb1da4744bf47d6b60480c0ddd742a7e7075
Shahil Pusarla <shpusarl@microsoft.com>
2019-12-18 18:52:29 +0530

    Add project files.

---
+++ WeatherForecast/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WeatherForecast.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            IEnumerable<WeatherForecast> weatherJson = null;
+            WeatherForecastEventSource.Log.Information("Start: Information raised from LogInformation action method of Home controller.");
+            try
+            {
+                var rng = new Random();
+                weatherJson = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            catch (Exception ex)
+            {
+                WeatherForecastEventSource.Log.Failure("Error raised from LogError action method of Home controller." + ex);
+            }
+
+            WeatherForecastEventSource.Log.Information("End: Information raised from LogInformation action method of Home controller.");
+            return weatherJson;
+        }
+    }
+}
---
+++ WeatherForecast/WeatherForecastEventSource.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace WeatherForecast
+{
+    [EventSource(Name = "Sample-EventSource")]
+    public class WeatherForecastEventSource : EventSource
+    {
+        public class Keywords
+        {
+            public const EventKeywords Application = (EventKeywords)1;
+            public const EventKeywords Service = (EventKeywords)2;
+            public const EventKeywords Database = (EventKeywords)4;
+        }
+
+        public class Tasks
+        {
+            public const EventTask Page = (EventTask)1;
+            public const EventTask DBQuery = (EventTask)2;
+        }
+
+        private static WeatherForecastEventSource _log = new WeatherForecastEventSource();
+        private WeatherForecastEventSource()
+        { }
+
+        public static WeatherForecastEventSource Log
+        {
+            get
+            {
+                return _log;
+            }
+        }
+
+        [Event(101, Keywords = Keywords.Application,
+            Level = EventLevel.Critical,
+            Message = "Application failure: {0} ")]
+        public void Failure(string message)
+        {
+            if (this.IsEnabled(EventLevel.Critical, Keywords.Application))
+            {
+                this.WriteEvent(101, message);
+            }
+        }
+
+        [Event(102, Keywords = Keywords.Application,
+            Level = EventLevel.Informational,
+            Message = "Information: {0}")]
+        public void Information(string message)
+        {
+            if (this.IsEnabled(EventLevel.Informational, Keywords.Application))
+            {
+                this.WriteEvent(102, message);
+            }
+        }
+    }
+}
